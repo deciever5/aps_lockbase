15a1514fddba74972d2a03f7cfc2b98d811ed458
deciever5 <marcinkunikowski@gmail.com>
2023-01-11 14:14:50 +0100

    added csv reader

--- app.py
+++ app.py
@@ -1,37 +1,46 @@
-from flask import Flask, render_template, request
+import pandas as pd
 from PyPDF2 import PdfReader
-import csv
-
-from logic import locks, get_locks
-
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from lock import create_locklist
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'archive/'
+app.config['ALLOWED_EXTENSIONS'] = {'pdf', 'csv'}
 
 
-@app.route('/', methods=['GET', 'POST'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/', methods=['GET'])
 def index():
-
     return render_template('index.html')
+
 
 @app.route('/upload-files', methods=['POST'])
 def upload_files():
     pdf_file = request.files.get('pdf-file')
     csv_file = request.files.get('csv-file')
-    if not pdf_file or not csv_file:
-        return 'Please select both pdf and csv file', 400
-    pdf = PdfReader(pdf_file)
-    # Extract the text from the PDF file
-    text = ''
-    for page in pdf.pages:
-        text += " " + page.extractText()
-    if csv_file:
-        csv_reader = csv.reader(csv_file.stream.read().decode("utf-8").splitlines())
-        for line in csv_reader:
-            print(line)
+    if pdf_file and allowed_file(pdf_file.filename) and csv_file and allowed_file(csv_file.filename):
+        pdf_filename = secure_filename(pdf_file.filename)
+        csv_filename = secure_filename(csv_file.filename)
+        pdf_file.save(app.config['UPLOAD_FOLDER'] + pdf_filename)
+        csv_file.save(app.config['UPLOAD_FOLDER'] + csv_filename)
 
-    return render_template('text.html', text=text )
+        # Extract the text from the PDF file
+        pdf_file = open(app.config['UPLOAD_FOLDER'] + pdf_filename, 'rb')
+        pdf_reader = PdfReader(pdf_file)
+        text = ''
+        for page_num in range(pdf_reader.numPages):
+            text += pdf_reader.getPage(page_num).extractText() + '\n'
+        pdf_file.close()
 
-
+        df = pd.read_csv(app.config['UPLOAD_FOLDER'] + csv_filename, delimiter=';', encoding='ANSI')
+        df.iloc[:, 2] = df.iloc[:, 2].str.slice(stop=58)
+        df = df.iloc[6:, [2, 3, 7, 11, 13, 14, 17, 18]]
+        create_locklist(df)
+        return render_template('text.html', text=text, data=df.to_html(max_rows=33, header=False))
 
 
 if __name__ == "__main__":
---
+++ lock.py
@@ -0,0 +1,86 @@
+class Lock:
+    def __init__(self, number, lock_type, length, finish, profile, quantity, pinning, body_pins, side_pins, ext_pins,
+                 pin_sum):
+        self._number = number
+        self._lock_type = lock_type
+        self._length = length
+        self._finish = finish
+        self._profile = profile
+        self._quantity = quantity
+        self._pinning = pinning
+        self._body_pins = body_pins
+        self._side_pins = side_pins
+        self._ext_pin = ext_pins
+        self._pin_sum = pin_sum
+
+    """def __str__(self):
+        return f'Number: {self._number}, Lock Type: {self._lock_type}, Length: {self._length}, Finish: {self._finish}, ' \
+               f'Profile: {self._profile}, Quantity: {self._quantity}, Body Pins: {self._body_pins}, Side Pins: {self._side_pins}, Extension Pins: {self._ext_pin} '"""
+
+    def __str__(self):
+        return f'Number: {self._number}, Body Pins: {self._body_pins}, Side Pins: {self._side_pins}, Extension Pins: {self._ext_pin} '
+
+    @property
+    def number(self):
+        return self._number
+
+    @property
+    def lock_type(self):
+        return self._lock_type
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def finish(self):
+        return self._finish
+
+    @property
+    def profile(self):
+        return self._profile
+
+    @property
+    def quantity(self):
+        return self._quantity
+
+    @property
+    def pinning(self):
+        return self._pinning
+
+    @property
+    def body_pins(self):
+        return self._body_pins
+
+    @property
+    def side_pins(self):
+        return self._side_pins
+
+    @property
+    def pin(self):
+        return self._ext_pin
+
+
+def create_locklist(df):
+    lock_list = []
+    for i in range(df.shape[0]):
+        number = df.iloc[i, 6]
+        lock_type = df.iloc[i, 7]
+        length = df.iloc[i, 2]
+        finish = df.iloc[i, 1]
+        profile = df.iloc[i, 4]
+        quantity = df.iloc[i, 5]
+        pinning = str(df.iloc[i, 3]).replace('\r', '').split('\n')
+        body_pins = pinning[0].split(' ')[0] if pinning[0].split(' ')[0].isnumeric() else None
+        side_pins = pinning[0].split(' ')[1] if pinning[0].split(' ')[0].isnumeric() else None
+        ext_pin = pinning[1:] if len(pinning) > 0 else None
+        pin_sum = None
+
+        lock = Lock(number, lock_type, length, finish, profile, quantity, pinning, body_pins, side_pins, ext_pin,
+                    pin_sum)
+        lock_list.append(lock)
+
+    for lock in lock_list:
+        print(lock)
+
+    return lock_list
