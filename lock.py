767eb402d21e0a804794b94bfbd9838b8bfe0382
deciever5 <marcinkunikowski@gmail.com>
2023-01-12 14:36:43 +0100

    added extension pin recountin data preparation

--- lock.py
+++ lock.py
@@ -13,9 +13,9 @@ class Lock:
         self._ext_pin = ext_pins
         self._pin_sum = pin_sum
 
-    """def __str__(self):
-        return f'Number: {self._number}, Lock Type: {self._lock_type}, Length: {self._length}, Finish: {self._finish}, ' \
-               f'Profile: {self._profile}, Quantity: {self._quantity}, Body Pins: {self._body_pins}, Side Pins: {self._side_pins}, Extension Pins: {self._ext_pin} '"""
+    """def __str__(self): return f'Number: {self._number}, Lock Type: {self._lock_type}, Length: {self._length}, 
+    Finish: {self._finish}, ' \ f'Profile: {self._profile}, Quantity: {self._quantity}, Body Pins: {self._body_pins}, 
+    Side Pins: {self._side_pins}, Extension Pins: {self._ext_pin} ' """
 
     def __str__(self):
         return f'Number: {self._number}, Body Pins: {self._body_pins}, Side Pins: {self._side_pins}, Extension Pins: {self._ext_pin} '
@@ -73,14 +73,24 @@ def create_locklist(df):
         pinning = str(df.iloc[i, 3]).replace('\r', '').split('\n')
         body_pins = pinning[0].split(' ')[0] if pinning[0].split(' ')[0].isnumeric() else None
         side_pins = pinning[0].split(' ')[1] if pinning[0].split(' ')[0].isnumeric() else None
-        ext_pin = pinning[1:] if len(pinning) > 0 else None
+        ext_pin = count_ext_pins(pinning[1:], body_pins) if len(pinning) > 1 else None
         pin_sum = None
 
         lock = Lock(number, lock_type, length, finish, profile, quantity, pinning, body_pins, side_pins, ext_pin,
                     pin_sum)
         lock_list.append(lock)
 
-    for lock in lock_list:
-        print(lock)
+    """for lock in lock_list:
+        print(lock)"""
 
     return lock_list
+
+
+def count_ext_pins(ext_pins, body_pins):
+    body_pins_list = [i for i in body_pins]
+    ext_pins_list = [[x.replace("a", "10").replace("b", "11").replace(" ", "0") for x in k] for k in
+                     [list(j) for j in [k for k in ext_pins]]]
+
+    print(body_pins_list, ext_pins_list)
+
+    return ext_pins_list
